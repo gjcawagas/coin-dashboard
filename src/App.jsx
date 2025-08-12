4e4718f7260fca4a7789bd63e83fcab6419b53d5
gjcawagas <gjcawagas@ndkc.edu.ph>
2025-08-12 16:16:19 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Hello, React + TypeScript + Bootstrap!</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
